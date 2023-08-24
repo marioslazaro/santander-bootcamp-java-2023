5a633da2a8e5dd8fa86960d96ad832594b566bb1
Mario Lazaro <mario-msl@hotmail.com>
2023-08-24 18:48:44 -0300

    Implementaçao da Camada de dominio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -1,13 +1,13 @@
-package me.dio.santanderbootcampjava2023;
+package me.dio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderBootcampJava2023Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBootcampJava2023Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
